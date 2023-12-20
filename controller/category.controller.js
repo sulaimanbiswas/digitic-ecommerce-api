5c4bf58b118383c6e954abb2e176d27ef97773e1
Sulaiman Biswas <sulaimanbiswasbd@gmail.com>
2023-12-21 05:49:22 +0600

    category update api

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -1,6 +1,8 @@
 const expressAsyncHandler = require("express-async-handler");
 const Category = require("../models/Category");
+const validateMongoDbId = require("../utils/validateMongoDbId");
 
+// create new category
 const createCategory = expressAsyncHandler(async (req, res) => {
   try {
     const category = await Category.create(req.body);
@@ -20,6 +22,30 @@ const createCategory = expressAsyncHandler(async (req, res) => {
   }
 });
 
+// update category by id
+const updateCategoryById = expressAsyncHandler(async (req, res) => {
+  const id = req.params.id;
+  validateMongoDbId(id);
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedCategory) {
+      return res.status(400).json({
+        success: false,
+        message: "Unable to update category",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Category updated successfully",
+      data: updatedCategory,
+    });
+  } catch (error) {}
+});
+
 module.exports = {
   createCategory,
+  updateCategoryById,
 };
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,4 +1,7 @@
-const { createCategory } = require("../controller/category.controller");
+const {
+  createCategory,
+  updateCategoryById,
+} = require("../controller/category.controller");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = require("express").Router();
 
@@ -7,7 +10,13 @@ const router = require("express").Router();
  * @route   POST /api/v1/category
  * @access  Private/Admin
  */
-
 router.post("/", authMiddleware, isAdmin, createCategory);
+
+/**
+ * @desc    Update category by id
+ * @route   PUT /api/v1/category/:id
+ * @access  Private/Admin
+ */
+router.put("/:id", authMiddleware, isAdmin, updateCategoryById);
 
 module.exports = router;
