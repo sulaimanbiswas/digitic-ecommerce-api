c2322196dd724b7af345c1fdca00c4d204dac76c
Sulaiman Biswas <sulaimanbiswasbd@gmail.com>
2023-12-24 07:37:01 +0600

    coupon api

--- controller/coupon.controller.js
+++ controller/coupon.controller.js
@@ -20,6 +20,70 @@ const createCoupon = expressAsyncHandler(async (req, res) => {
   }
 });
 
+// update coupon by id
+const updateCouponById = expressAsyncHandler(async (req, res) => {
+  const id = req.params.id;
+  validateMongoDbId(id);
+  try {
+    const updatedCoupon = await Coupon.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedCoupon) {
+      res.status(400);
+      throw new Error("Unable to update coupon");
+    }
+    res.status(200).json({
+      success: true,
+      message: "Coupon updated successfully",
+      data: updatedCoupon,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// get all coupons
+const getAllCoupons = expressAsyncHandler(async (req, res) => {
+  try {
+    const coupons = await Coupon.find({});
+    if (!coupons) {
+      res.status(400);
+      throw new Error("Unable to fetch coupons");
+    }
+    res.status(200).json({
+      success: true,
+      message: "Coupons fetched successfully",
+      data: coupons,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// delete coupon by id
+const deleteCouponById = expressAsyncHandler(async (req, res) => {
+  const id = req.params.id;
+  validateMongoDbId(id);
+  try {
+    const deletedCoupon = await Coupon.findByIdAndDelete(id);
+    if (!deletedCoupon) {
+      res.status(400);
+      throw new Error("Unable to delete coupon");
+    }
+    res.status(200).json({
+      success: true,
+      message: "Coupon deleted successfully",
+      data: deletedCoupon,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createCoupon,
+  updateCouponById,
+  getAllCoupons,
+  deleteCouponById,
 };
--- routes/coupon.route.js
+++ routes/coupon.route.js
@@ -1,4 +1,9 @@
-const { createCoupon } = require("../controller/coupon.controller");
+const {
+  createCoupon,
+  getAllCoupons,
+  updateCouponById,
+  deleteCouponById,
+} = require("../controller/coupon.controller");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = require("express").Router();
 
@@ -8,5 +13,26 @@ const router = require("express").Router();
  * @access  Private/Admin
  */
 router.post("/", authMiddleware, isAdmin, createCoupon);
+
+/**
+ * @desc    Update coupon by id
+ * @route   PUT /api/v1/coupon/:id
+ * @access  Private/Admin
+ */
+router.put("/:id", authMiddleware, isAdmin, updateCouponById);
+
+/**
+ * @desc    Get all coupons
+ * @route   GET /api/v1/coupon
+ * @access  Private/Admin
+ */
+router.get("/", authMiddleware, isAdmin, getAllCoupons);
+
+/**
+ * @desc    Delete coupon by id
+ * @route   DELETE /api/v1/coupon/:id
+ * @access  Private/Admin
+ */
+router.delete("/:id", authMiddleware, isAdmin, deleteCouponById);
 
 module.exports = router;
